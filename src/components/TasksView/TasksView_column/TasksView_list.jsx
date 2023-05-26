dd9ada0916e2687abffdb699cac65654e70dbeea
alirezaheibati <alirezaheibati91@gmail.com>
2023-05-26 09:40:57 +0430

    create and style header for column view tasks

--- src/components/TasksView/TasksView_column/TasksView_list.jsx
+++ src/components/TasksView/TasksView_column/TasksView_list.jsx
@@ -1,8 +1,17 @@
+import ColumnViewHeader from "./ColumnViewHeader";
+import classes from "./TasksView_list.module.css";
+const TasksViewColumn = () => {
+  return (
+    <div className={classes.container}>
+      <div className={classes.box}>
+        <ColumnViewHeader title="Open" count={"۰"} borderColor={"#F98F2E"} />
+      </div>
+      <div className={classes.box}>2</div>
+      <div className={classes.box}>3</div>
+      <div className={classes.box}>4</div>
+      <div className={classes.box}>5</div>
+    </div>
+  );
+};
 
-const TasksViewColumn = () =>{
-    return(<div>
-        show tasks in Column
-    </div>)
-}
-
-export default TasksViewColumn
+export default TasksViewColumn;
