a69ed212d4ec02533ec49c896cc8f972beef7791
alirezaheibati <alirezaheibati91@gmail.com>
2023-05-26 19:30:57 +0430

    task column view component created and styled

--- src/components/TasksView/TasksView_column/TasksView_list.jsx
+++ src/components/TasksView/TasksView_column/TasksView_list.jsx
@@ -1,15 +1,29 @@
 import ColumnViewHeader from "./ColumnViewHeader";
+import ColumnViewTask from "./ColumnViewTask";
 import classes from "./TasksView_list.module.css";
 const TasksViewColumn = () => {
   return (
     <div className={classes.container}>
       <div className={classes.box}>
         <ColumnViewHeader title="Open" count={"۰"} borderColor={"#F98F2E"} />
+        <ColumnViewTask />
       </div>
-      <div className={classes.box}>2</div>
-      <div className={classes.box}>3</div>
-      <div className={classes.box}>4</div>
-      <div className={classes.box}>5</div>
+      <div className={classes.box}>
+        <ColumnViewHeader
+          title="In progress"
+          count={"۰"}
+          borderColor={"#2E7FF9"}
+        />
+      </div>
+      <div className={classes.box}>
+        <ColumnViewHeader title="Pending" count={"۰"} borderColor={"#DEC908"} />
+      </div>
+      <div className={classes.box}>
+        <ColumnViewHeader title="To Do" count={"۰"} borderColor={"#F98F2E"} />
+      </div>
+      <div className={classes.box}>
+        <ColumnViewHeader title="Done" count={"۰"} borderColor={"#47A992"} />
+      </div>
     </div>
   );
 };
