3b4848b948deabbee7e0a8cbe9e8c278db0c5aa3
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2023-05-28 09:16:11 +0330

    Alirezazzz (#5)
    * create and style header for column view tasks
    
    * task column view component created and styled
    
    * backdrop component for rendering pop-ups has been created and styled
    
    * I have created the new task button but it should be move to layout component
    
    * task information componenet right side has designed and astyled
    
    * task info left part created and styled
    
    * taskInformation commponent functionality has been set

--- src/components/layout/homePage_sidebar/HomePageSidebar.jsx
+++ src/components/layout/homePage_sidebar/HomePageSidebar.jsx
@@ -1,11 +1,13 @@
-import "./HomePageSidebar_style.css"
+import "./HomePageSidebar_style.css";
 
-const HomePageSidebar = () =>{
-    return(
-        <div className="w-[24%] min-w-[300px] border-l border-slate-500 ">
-            <h1 className="headerTitle bg-clip-text text-[32px] font-extrabold">کوئرا تسک منیجر</h1>
-        </div>
-    )
-}
+const HomePageSidebar = () => {
+  return (
+    <div className="w-[24%] min-w-[300px] border-l border-slate-500 ">
+      <h1 className="headerTitle bg-clip-text text-[32px] font-extrabold">
+        کوئرا تسک منیجر
+      </h1>
+    </div>
+  );
+};
 
-export default HomePageSidebar
+export default HomePageSidebar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,32 +1,31 @@
 import { useContext } from "react";
-import Layout from "../components/layout/Layout"
+import Layout from "../components/layout/Layout";
 import TasksViewCalendar from "../components/TasksView/TasksView_calendar/TasksView_list";
 import TasksViewColumn from "../components/TasksView/TasksView_column/TasksView_list";
 import TasksViewList from "../components/TasksView/TasksView_list/TasksView_list";
 import { UserContext } from "../context/provider";
 
 const HomePage = () => {
+  const { taskManagerState } = useContext(UserContext);
 
-    const { taskManagerState } = useContext(UserContext);
-
-    const ShowTasksHandler = () => {
-        switch(taskManagerState){
-            case "list":
-                return <TasksViewList />
-            case "column":
-                return <TasksViewColumn />
-            case "calendar":
-                return <TasksViewCalendar />
-            default:
-                return
-        }
+  const ShowTasksHandler = () => {
+    switch (taskManagerState) {
+      case "list":
+        return <TasksViewList />;
+      case "column":
+        return <TasksViewColumn />;
+      case "calendar":
+        return <TasksViewCalendar />;
+      default:
+        return;
     }
+  };
 
-    return (
-        <Layout>
-            <ShowTasksHandler />
-        </Layout>
-    )
-}
+  return (
+    <Layout>
+      <ShowTasksHandler />
+    </Layout>
+  );
+};
 
-export default HomePage
+export default HomePage;
