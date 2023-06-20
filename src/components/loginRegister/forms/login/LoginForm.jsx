2a06da6eb20c1ee002a4f0d0e4dea8dafdb25890
ali javadpour <alijavadpour@alis-MacBook-Pro.local>
2023-06-20 18:03:16 +0330

    some api hook change

--- src/components/loginRegister/forms/login/LoginForm.jsx
+++ src/components/loginRegister/forms/login/LoginForm.jsx
@@ -1,4 +1,6 @@
-import { useReducer } from "react";
+import { useContext, useReducer } from "react";
+import { UserContext } from "../../../../context/provider";
+import useHttp from "../../../../hooks/use-http";
 import Input from "../premades/Input";
 
 let regex = new RegExp("[a-z0-9]+@[a-z]+[.][a-z]{2,3}");
@@ -8,13 +10,13 @@ const loginFormReducer = (state, action) => {
     return { ...state, emailIsNotValid: action.validity };
   }
   if (action.type === "EMAIL_CHANGE") {
-    return { ...state, emailIsNotValid: action.validity };
+    return { ...state, emailIsNotValid: action.validity, emailInputValue: action.value };
   }
   if (action.type === "PASSWORD_BLUR") {
     return { ...state, passwordIsNotValid: action.validity };
   }
   if (action.type === "PASSWORD_CHANGE") {
-    return { ...state, passwordIsNotValid: action.validity };
+    return { ...state, passwordIsNotValid: action.validity, passwordInputValue: action.value };
   }
   if (action.type === "FORM_VALIDITY") {
     return { ...state, loginFormIsValid: action.validity };
@@ -29,6 +31,9 @@ const loginFormReducer = (state, action) => {
 };
 
 const LoginForm = (props) => {
+
+  const contextData = useContext(UserContext)
+
   const [loginFormState, dispatchLoginForm] = useReducer(loginFormReducer, {
     emailIsNotValid: undefined,
     emailInputValue: "",
@@ -52,14 +57,15 @@ const LoginForm = (props) => {
   };
 
   const emailChangeHandler = (e) => {
-    dispatchLoginForm({ type: "EMAIL_CHANGE", validity: false });
+    dispatchLoginForm({ type: "EMAIL_CHANGE", validity: false, value: e.target.value });
     if (regex.test(e.target.value.toLowerCase())) {
       dispatchLoginForm({
         type: "FORM_VALIDITY",
         validity: !(loginFormState.passwordIsNotValid ?? true),
+        value: e.target.value
       });
     } else {
-      dispatchLoginForm({ type: "FORM_VALIDITY", validity: false });
+      dispatchLoginForm({ type: "FORM_VALIDITY", validity: false, value: e.target.value });
     }
   };
 
@@ -70,25 +76,49 @@ const LoginForm = (props) => {
   };
 
   const passwordChangeHandler = (e) => {
-    dispatchLoginForm({ type: "PASSWORD_CHANGE", validity: false });
+    dispatchLoginForm({ type: "PASSWORD_CHANGE", validity: false, value: e.target.value });
     if (e.target.value.trim().length >= 8) {
       dispatchLoginForm({
         type: "FORM_VALIDITY",
         validity: !(loginFormState.emailIsNotValid ?? true),
+        value: e.target.value
       });
     } else {
-      dispatchLoginForm({ type: "FORM_VALIDITY", validity: false });
+      dispatchLoginForm({ type: "FORM_VALIDITY", validity: false, value: e.target.value });
     }
   };
 
+  const loginUserRequest = (res) =>{
+    console.log(res);
+    contextData.setIsLogin(true)
+  }
+
+  const { sendRequest:loginRequest } = useHttp(
+    {
+      url: "http://localhost:3000/api/auth/login",
+      method: "POST",
+      body: {
+        emailOrUsername: loginFormState.emailInputValue,
+        password: loginFormState.passwordInputValue,
+      },
+    },
+    loginUserRequest
+  );
+
+  const onSubmitHandler = (e) =>{
+    e.preventDefault();
+    loginRequest()
+  }
+
   return (
-    <form className="">
+    <form className="" onSubmit={onSubmitHandler}>
       <h2 className="text-center text-2xl mb-8">{`(: به کوئرا تسک منیجر خوش برگشتی`}</h2>
       <Input
         title="ایمیل"
         id="login-form__email"
         inputIsNotValid={loginFormState.emailIsNotValid}
         type="text"
+        value = {loginFormState.emailInputValue}
         inputBlurHandler={emailBlurHandler}
         inputChangeHandler={emailChangeHandler}
         placeholder="example@sth.sth"
@@ -98,6 +128,7 @@ const LoginForm = (props) => {
         id="login-form__password"
         inputIsNotValid={loginFormState.passwordIsNotValid}
         type="text"
+        value = {loginFormState.passwordInputValue}
         inputBlurHandler={passwordBlurHandler}
         inputChangeHandler={passwordChangeHandler}
       />
--- src/hooks/use-http.jsx
+++ src/hooks/use-http.jsx
@@ -1,19 +1,21 @@
 import { useContext } from "react";
+import AuthContext from "../context/auth-context";
 import SpinnerContext from "../context/spinner-context";
 
 const useHttp = (requestConfig, applyData) => {
   const spinnerCtx = useContext(SpinnerContext);
+  const authCtx = useContext(AuthContext);
 
   const sendRequest = async () => {
     spinnerCtx.toggleSpinner();
     try {
       const response = await fetch(requestConfig.url, {
         method: requestConfig.method ? requestConfig.method : "GET",
-        headers: requestConfig.headers ? requestConfig.headers : {},
+        headers: { "Content-Type": "application/json", "x-auth-token": authCtx.accessToken },
         body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
       });
       if (!response.ok) {
-        if (response.statusText === "Conflict") {
+        if (response.statusText === "Conflict" || response.status === 404) {
           const data = await response.json();
           throw new Error(data.message);
         } else {
@@ -22,6 +24,26 @@ const useHttp = (requestConfig, applyData) => {
           );
         }
       }
+
+      // if(response){
+      //   console.log(response);
+      //   const data = await response.json();
+      //   console.log(data);
+      //   if(!response.ok){
+      //     if(data.message){
+      //       throw new Error(data.message);
+      //     }else{
+      //       throw new Error(
+      //         "someThing went wrong please try a few seconds later."
+      //       );
+      //     }
+      //   }
+      // }else{
+      //   throw new Error(
+      //     "check your internet connection."
+      //   );
+      // }
+       
       const data = await response.json();
 
       applyData(data);
