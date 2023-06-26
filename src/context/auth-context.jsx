55823201382d1d47d403b1d9a88303e3940b7657
alirezaheibati <alirezaheibati91@gmail.com>
2023-06-26 16:35:04 +0330

    auth context has been modified to save user data as a new state

--- src/context/auth-context.jsx
+++ src/context/auth-context.jsx
@@ -5,11 +5,14 @@ const AuthContext = React.createContext({
   refreshToken: "",
   userID: "",
   isLoggedIn: false,
+  userData: {},
   login: (accessToken, refreshToken, userId) => {},
   logout: () => {},
+  userDataManager: (data) => {},
 });
 
 export const AuthContextProvider = (props) => {
+  const [userData, setUserData] = useState({});
   const [accessToken, setAccessToken] = useState(null);
   const [refreshToken, setRefreshToken] = useState(null);
 
@@ -29,13 +32,20 @@ export const AuthContextProvider = (props) => {
     setUserID("");
   };
 
+  const userDataManager = (data) => {
+    setUserData(data);
+    console.log(data);
+  };
+
   const contextValue = {
     accessToken: accessToken,
     refreshToken: refreshToken,
     userID: userID,
+    userData: userData,
     isLoggedIn: userIsLoggedIn,
     login: loginHandler,
     logout: logoutHandler,
+    userDataManager: userDataManager,
   };
 
   return (
